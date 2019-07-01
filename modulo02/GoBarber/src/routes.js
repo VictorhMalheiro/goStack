45c10bbfe93814ca00c68962bc750f3b3a397a7b
Victor Malheiro <victorhugobarretos@gmail.com>
2019-07-01 13:05:21 -0300

    GoBarber - Criando loader de models

--- modulo02/GoBarber/src/routes.js
+++ modulo02/GoBarber/src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello Victor' });
+routes.get('/', async (req, res) => {
+    const user = await User.create({
+        name: 'Victor Malheiro',
+        email: 'victor@virtualmake.net',
+        password_hash: '123456',
+    });
+
+    return res.json(user);
 });
 
 export default routes;
