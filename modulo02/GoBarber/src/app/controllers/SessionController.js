87092236baeba5476fe9e3fc734ebacbc46ac8b1
Victor Malheiro <victorhugobarretos@gmail.com>
2019-07-03 17:54:07 -0300

    Update do usuário

--- modulo02/GoBarber/src/app/controllers/SessionController.js
+++ modulo02/GoBarber/src/app/controllers/SessionController.js
@@ -1,47 +1,40 @@
 import jwt from 'jsonwebtoken';
 
+import authConfig from '../../config/auth';
 import User from '../models/User';
 
 class SessionController {
     async store(req, res) {
-        const {
-            email,
-            password
-        } = req.body;
+        const { email, password } = req.body;
 
         const user = await User.findOne({
             where: {
-                email
-            }
+                email,
+            },
         });
 
         if (!user) {
             return res.status(401).json({
-                error: 'User not found'
+                error: 'User not found',
             });
         }
         if (!(await user.checkPassword(password))) {
             return res.status(401).json({
-                error: 'Passoword does not match'
+                error: 'Passoword does not match',
             });
         }
 
-        const {
-            id,
-            name
-        } = user;
+        const { id, name } = user;
         return res.json({
             user: {
                 id,
                 name,
                 email,
             },
-            token: jwt.sign({
-                id
-            }, '449c8e5c07fe2958f74fa2ca1f9c54f9', {
-                expiresIn: '7d',
+            token: jwt.sign({ id }, authConfig.secret, {
+                expiresIn: authConfig.expiresIn,
             }),
-        })
+        });
     }
 }
 
---
+++ modulo02/GoBarber/src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+    secret: '449c8e5c07fe2958f74fa2ca1f9c54f9',
+    expiresIn: '7d',
+};
