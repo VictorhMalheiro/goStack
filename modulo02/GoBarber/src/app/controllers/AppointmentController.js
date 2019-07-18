e83f550babccf51a300beefe8a969d05491facf9
Victor <victorhugobarretos@gmail.com>
2019-07-18 08:39:57 -0300

    Schedule and Appointments

--- modulo02/GoBarber/src/app/controllers/AppointmentController.js
+++ modulo02/GoBarber/src/app/controllers/AppointmentController.js
@@ -1,8 +1,37 @@
 import * as Yup from 'yup';
-import Appointment from '../models/Appointment';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
 import User from '../models/User';
+import File from '../models/File';
+import Appointment from '../models/Appointment';
 
 class AppointmentController {
+    async index(req, res) {
+        const { page = 1 } = req.query;
+
+        const appointments = await Appointment.findAll({
+            where: { user_id: req.userId, canceled_at: null },
+            order: ['date'],
+            attributes: ['id', 'date'],
+            limit: 20,
+            offset: (page - 1) * 20,
+            include: [
+                {
+                    model: User,
+                    as: 'provider',
+                    attributes: ['id', 'name'],
+                    include: [
+                        {
+                            model: File,
+                            as: 'avatar',
+                            attributes: ['id', 'path', 'url'],
+                        },
+                    ],
+                },
+            ],
+        });
+        return res.json(appointments);
+    }
+
     async store(req, res) {
         const schema = Yup.object().shape({
             provider_id: Yup.number().required(),
@@ -24,6 +53,34 @@ class AppointmentController {
             },
         });
 
+        /*
+            Check for past dates
+        */
+        const hourStart = startOfHour(parseISO(date));
+
+        if (isBefore(hourStart, new Date())) {
+            return res
+                .status(400)
+                .json({ error: 'Past dates are not permited' });
+        }
+
+        /*
+            Check date availability
+        */
+        const checkAvailability = await Appointment.findOne({
+            where: {
+                provider_id,
+                canceled_at: null,
+                date: hourStart,
+            },
+        });
+
+        if (checkAvailability) {
+            return res
+                .status(400)
+                .json({ error: ' Appointment date is not available' });
+        }
+
         if (!isProvider) {
             return res.status(401).json({
                 error: 'You can only create appointments with providers',
@@ -31,7 +88,7 @@ class AppointmentController {
         }
 
         const appointment = await Appointment.create({
-            user_id: req.user_id,
+            user_id: req.userId,
             provider_id,
             date,
         });
--- modulo02/GoBarber/src/routes.js
+++ modulo02/GoBarber/src/routes.js
@@ -8,6 +8,7 @@ import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
 import authMiddleware from './app/middlewares/auth';
+import ScheduleController from './app/controllers/ScheduleController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -20,6 +21,10 @@ routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
 
+routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
+
+routes.get('/schedule', ScheduleController.index);
+
 routes.post('/files', upload.single('file'), FileController.store);
 export default routes;
