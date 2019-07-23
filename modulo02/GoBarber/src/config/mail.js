500633f3ed0c74b67d312826f1376dff122de0f4
Victor <victorhugobarretos@gmail.com>
2019-07-23 20:08:32 -0300

    Configurando Nodemailer

---
+++ modulo02/GoBarber/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+    host: 'smtp.mailtrap.io',
+    port: 2525,
+    secure: false,
+    auth: {
+        user: '576d9e15dfb2e7',
+        pass: '4a1b4c9ca00a6c',
+    },
+    default: {
+        from: 'Equipe GoBarber <noreplay@gobarber.com>',
+    },
+};
