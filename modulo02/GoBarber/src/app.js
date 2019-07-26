3bb8b41d1f5f65211ad19c6d6ac5a1bd49564f36
Victor <victorhugobarretos@gmail.com>
2019-07-25 21:59:26 -0300

    Tratamento de exceções 0.1

--- modulo02/GoBarber/src/app.js
+++ modulo02/GoBarber/src/app.js
@@ -1,6 +1,10 @@
 import express from 'express';
 import path from 'path';
+import * as Sentry from '@sentry/node';
 import routes from './routes';
+import sentryConfig from './config/sentry';
+
+import 'express-async-errors';
 
 import './database';
 
@@ -8,11 +12,14 @@ class App {
     constructor() {
         this.server = express();
 
+        Sentry.init(sentryConfig);
+
         this.middlewares();
         this.routes();
     }
 
     middlewares() {
+        this.server.use(Sentry.Handlers.requestHandler());
         this.server.use(express.json());
         this.server.use(
             '/files',
@@ -22,6 +29,7 @@ class App {
 
     routes() {
         this.server.use(routes);
+        this.server.use(Sentry.Handlers.errorHandler());
     }
 }
 
---
+++ modulo02/GoBarber/src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+    dns: 'https://680226288a1e43a18ea4cf7aff9a2e56@sentry.io/1514038',
+};
