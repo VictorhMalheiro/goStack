e3584070ab8081bbff77cc880ebace32e8c8e0f2
Victor Hugo Malheiro <victorhugobarretos@gmail.com>
2019-09-03 18:04:47 -0300

    Adicionando repositórios

--- modulo05/src/pages/Main/styles.js
+++ modulo05/src/pages/Main/styles.js
@@ -32,9 +32,10 @@ export const Form = styled.form`
     font-size: 16px;
   }
 `;
-export const SubmitButton = styled.button.attrs({
-  type: 'submit'
-})`
+export const SubmitButton = styled.button.attrs( props => ({
+  type: 'submit',
+  disabled: props.loading,
+}))`
   background: #7159ce;
   border: 0;
   padding: 0 15px;
@@ -44,4 +45,9 @@ export const SubmitButton = styled.button.attrs({
   display: flex;
   justify-content: center;
   align-items: center;
+
+  &[disabled] {
+    cursor: not-allowed;
+    opacity: 0.6;
+  }
 `;
