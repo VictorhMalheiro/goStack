f8d4a392d3f89b4935aafb246069654a40db5d4b
Victor Hugo Malheiro <victorhugobarretos@gmail.com>
2019-09-03 00:10:50 -0300

    Estilizando página Main

--- modulo05/src/pages/Main/index.js
+++ modulo05/src/pages/Main/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
 
-import { Title } from './styles';
+import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+import { Container, Form, SubmitButton } from './styles';
 
 function Main() {
   return (
-    <Title>
-      Hello World
-    </Title>
+    <Container>
+      <h1>
+        <FaGithubAlt />
+        Repositórios
+      </h1>
+
+      <Form onSubmit={() => {}}>
+        <input type="text" placeholder="Adicionar repositório" />
+
+        <SubmitButton disabled>
+          <FaPlus color="#fff" size="14" />
+        </SubmitButton>
+      </Form>
+    </Container>
   );
 }
 
