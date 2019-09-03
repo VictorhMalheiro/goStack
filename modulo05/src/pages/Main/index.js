33cc1041dd37bc5409c926ddcc6a6ebfd044c644
Victor Hugo Malheiro <victorhugobarretos@gmail.com>
2019-09-02 23:03:33 -0300

    Styled-Components

--- modulo05/src/pages/Main/index.js
+++ modulo05/src/pages/Main/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
+import { Title } from './styles';
+
 function Main() {
-  return <h1>Main</h1>;
+  return (
+    <Title error={false}>
+      Main
+      <small>Ok Goolge</small>
+    </Title>
+  );
 }
 
 export default Main;
