3eb0d6e30e6a2d510829be7778ac9e14220cab4e
Victor Hugo Malheiro <victorhugobarretos@gmail.com>
2019-10-10 17:40:06 -0300

    Acessando API do Github

--- modulo06/src/config/ReactotronConfig.js
+++ modulo06/src/config/ReactotronConfig.js
@@ -2,6 +2,7 @@ import Reactotron from 'reactotron-react-native';
 
 if (__DEV__) {
   const tron = Reactotron.configure({ host: '192.168.3.159' })
+    // const tron = Reactotron.configure()
     .useReactNative()
     .connect();
   console.tron = tron;
