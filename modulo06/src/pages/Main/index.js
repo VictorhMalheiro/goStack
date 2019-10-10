c37339535163c852040d590753fd715bc649429b
Victor Hugo Malheiro <victorhugobarretos@gmail.com>
2019-10-10 13:02:23 -0300

    React Navigation

---
+++ modulo06/src/pages/Main/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import { Container, Form, Input, SubmitButton } from './styles';
+
+export default function Main() {
+  return (
+    <Container>
+      <Form>
+        <Input
+          autoCorrect={false}
+          autoCaptaliz="none"
+          placeholder="Adicionar usuário"
+        />
+        <SubmitButton>
+          <Icon name="add" size={20} color="#FFF" />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
+}
+
+Main.navigationOptions = {
+  title: 'Usuários',
+};
