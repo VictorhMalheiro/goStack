75a1f12a1ba2edc14acf7a5a3536d60277228e27
Victor Hugo Malheiro <victorhugobarretos@gmail.com>
2019-10-11 18:08:04 -0300

    Buscando dados da API 0.1

--- modulo06/src/pages/User/index.js
+++ modulo06/src/pages/User/index.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View } from 'react-native';
+import api from '../../services/api';
 
 // import { Container } from './styles';
 
-export default function User({ navigation }) {
+export default class User extends Component {
+  static navigationOptions = ({ navigation }) => ({
+    title: navigation.getParam('user').name,
+  });
 
+  state = {
+    stars: [],
+  };
 
-  return <View />;
+  async componentDidMount() {
+    const { navigation } = this.props;
+    const user = navigation.getParam('user');
+
+    const response = await api.get(`/users/${user.login}/starred`);
+
+    this.setState({ stars: response.data });
+  }
+
+  render() {
+    return <View />;
+  }
 }
