02f69fa3943a87fc2894939b3fd610b1b91b26ac
Victor Hugo Malheiro <victorhugobarretos@gmail.com>
2019-10-14 10:37:47 -0300

    Listando favoritos

--- modulo06/src/pages/User/index.js
+++ modulo06/src/pages/User/index.js
@@ -1,13 +1,32 @@
+/* eslint-disable react/state-in-constructor */
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import PropTypes from 'prop-types';
 import api from '../../services/api';
 
-// import { Container } from './styles';
+import {
+  Container,
+  Header,
+  Avatar,
+  Name,
+  Bio,
+  Stars,
+  Starred,
+  OwnerAvatar,
+  Info,
+  Title,
+  Author,
+} from './styles';
 
 export default class User extends Component {
   static navigationOptions = ({ navigation }) => ({
     title: navigation.getParam('user').name,
   });
+
+  static propTypes = {
+    navigation: PropTypes.shape({
+      getParam: PropTypes.func,
+    }).isRequired,
+  };
 
   state = {
     stars: [],
@@ -23,6 +42,31 @@ export default class User extends Component {
   }
 
   render() {
-    return <View />;
+    const { navigation } = this.state;
+    const { stars } = this.state;
+
+    const user = navigation.getParam('user');
+    return (
+      <Container>
+        <Header>
+          <Avatar source={{ uri: user.avatar }} />
+          <Name>{user.name}</Name>
+          <Bio>{user.bio}</Bio>
+        </Header>
+        <Stars
+          data={stars}
+          keyExtractor={star => String(star.id)}
+          renderItem={({ item }) => (
+            <Starred>
+              <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
+              <Info>
+                <Title>{item.name}</Title>
+                <Author>{item.owner.login}</Author>
+              </Info>
+            </Starred>
+          )}
+        />
+      </Container>
+    );
   }
 }
