432d85ffa1d46c0fec6ba597cfce3764fbd31e62
Victor Hugo Malheiro <victorhugobarretos@gmai.com>
2020-01-05 13:24:42 -0300

    Alterando quantidade no carrinho

--- modulo07/src/pages/Cart/index.js
+++ modulo07/src/pages/Cart/index.js
@@ -1,15 +1,24 @@
 import React from "react";
 import { connect } from "react-redux";
-import * as CartActions from "../../store//modules/cart/actions";
 import { bindActionCreators } from "redux";
 import {
   MdRemoveCircleOutline,
   MdAddCircleOutline,
   MdDelete
 } from "react-icons/md";
+
+import * as CartActions from "../../store//modules/cart/actions";
+
 import { Container, ProductTable, Total } from "./styles";
 
-function Cart({ cart, removeFromCart }) {
+function Cart({ cart, removeFromCart, updateAmount }) {
+  function increment(product) {
+    updateAmount(product.id, product.amount + 1);
+  }
+
+  function decrement(product) {
+    updateAmount(product.id, product.amount - 1);
+  }
   return (
     <Container>
       <ProductTable>
@@ -34,11 +43,11 @@ function Cart({ cart, removeFromCart }) {
               </td>
               <td>
                 <div>
-                  <button type="button">
+                  <button type="button" onClick={() => decrement(product)}>
                     <MdRemoveCircleOutline color="#7159c1" />
                   </button>
                   <input type="number" readOnly value={product.amount} />
-                  <button type="button">
+                  <button type="button" onClick={() => increment(product)}>
                     <MdAddCircleOutline color="#7159c1" />
                   </button>
                 </div>
@@ -49,9 +58,7 @@ function Cart({ cart, removeFromCart }) {
               <td>
                 <button
                   type="button"
-                  onClick={() =>
-                    removeFromCart(product.id)
-                  }
+                  onClick={() => removeFromCart(product.id)}
                 >
                   <MdDelete size={20} color="#7159c1" />
                 </button>
